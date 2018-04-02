877e263731e8ba610d966419c2322b92351dc7be
Aman Maldar <amanmaldar@users.noreply.github.com>
2018-04-02 19:56:45 -0400

    new

---
+++ main.cpp
@@ -0,0 +1,25 @@
+//new
+#include <iostream>
+#include <cstdlib>
+#include <set>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+	vector <int> data;
+	int lenght = 100;
+	int sum = 0;
+	for(int i = 0; i < lenght ; i++){
+		data.at(i) = (rand % 10) +1;	
+	}
+
+	for(int i = 0; i < lenght ; i++){
+		sum += data.at(i);	
+	}
+	cout << "sum = " << sum << endl;
+
+
+
+	return 0;
+}
