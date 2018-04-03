4dccda90d611a5486b9877926f979e6b0fefb065
Aman Maldar <amanmaldar@users.noreply.github.com>
2018-04-02 21:39:51 -0400

    functions added

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
 # LinuxPerf
 
 
-Compilation with gdb. We need symbol table as well. <br />
-`g++ -std=c++11 -ggdb main.cpp -o linuxPerf`
+Compilation with gdb. We need symbol table as well.  <br />
+Symbols: -O0 = bigO, zero 
+`g++ -std=c++11 -ggdb  –O0 –g main.cpp -o linuxPerf`
 
 This command automatically creates default file perf.data <br />
-`sudo perf record -e cpu-clock,faults ./linuxPerf` 
+`sudo perf record -g -e cpu-clock,faults ./linuxPerf` 
+`perf report`
+Sumbols: -g = hirarchical profiling
+
+`perf report -G`
 `perf report`
 
-
+Ref: https://stackoverflow.com/questions/27742462/understanding-linux-perf-report-output?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa <br />
--- main.cpp
+++ main.cpp
@@ -5,29 +5,63 @@
 #include <ctime>
 using namespace std;
 
+
+void bar();
+void foo();
+vector <int> data;
+vector <int> ans;
+int lenght = 20000;
+int sum = 0;
+
+
+void foo(void) {
+	for(int i = 0; i < lenght ; i++){
+	sum = 0;
+		for(int j = 0; j < lenght ; j++){
+			sum += data.at(j);			
+		}	
+			ans.push_back(sum);		//this will be same all the time
+	}
+}
+
+
+void bar(void) {
+   
+   	for(int i = 0; i < lenght ; i++){
+	sum = 0;
+		for(int j = 0; j < lenght ; j++){
+			sum += data.at(j);			
+		}	
+			ans.push_back(sum);		//this will be same all the time
+	}
+   
+    foo();		//calls foo
+}
+
+
 int main()
 {
-	vector <int> data;
-	vector <int> ans;
- 	int lenght = 50000;
-	int sum = 0;
+
 	for(int i = 0; i < lenght ; i++){
 		data.push_back((rand() % 5) + 1);	
 	}
+	
+	bar();
+
+	
+
+	return 0;
+}
+
+
+
+
+/*
 
 	for(int i = 0; i < lenght ; i++){
 		sum += data.at(i);	
 	}
 	cout << "sum = " << sum << endl;
 
-	for(int i = 0; i < lenght ; i++){
-		sum = 0;
-		for(int j = 0; j < lenght ; j++){
-			sum += data.at(j);			
-		}	
-		ans.push_back(sum);		//this will be same all the time
-	}
-	
 
-	return 0;
-}
+*/
